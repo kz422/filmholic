a0ffa8dd6b4e48aae94c400e34785097d71c6899
kz422 <p.inoue.p@gmail.com>
2021-09-03 22:08:32 +0900

    general

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ActorInfo from './components/ActorInfo/ActorInfo';
+import Footer from './components/Footer';
+
+import Header from './components/Header';
+import Home from './components/Home';
+import Movie from './components/Movie';
+import NotFound from './components/NotFound';
+
+import { GlobalStyle } from './GlobalStyle'
+
+const App = () =>  (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/movie/:movieId' element={<Movie />} />
+        <Route path='/actor/:actorId' element={<ActorInfo />} />
+        <Route path='/*' element={<NotFound />} />
+      </Routes>
+      <GlobalStyle />
+      {/* <Footer /> */}
+    </Router>
   );
-}
+
 
 export default App;
